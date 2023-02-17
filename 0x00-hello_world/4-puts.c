f1eef3259ee1b899b6e5a96fba8d8f1935ad8ca8
ActuarialDataAnalyst <lesleymathe95@gmail.com>
2023-02-16 16:07:01 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line"
+ * Return 0.
  */
 
 int main(void)
